01a826065408aa1a3a75ed3461ba4880972ee245
May <may.johansson@hva.nl>
2019-02-06 14:11:27 +0100

    list page done, using fetch

--- week1/js/app.js
+++ week1/js/app.js
@@ -1,43 +1,66 @@
 'use strict'
 
-var request = new XMLHttpRequest();
+const url = 'https://ghibliapi.herokuapp.com/films'
+const app = document.getElementById('main')
 
-request.open('GET', 'https://ghibliapi.herokuapp.com/films', true);
+fetch(url)
+.then((resp) => resp.json())
+.then(function(data) {
 
-request.onload = function () {
+  let movies = data
 
-  // Begin accessing JSON data here
-  var data = JSON.parse(this.response);
+  return movies.map(function(movie) {
+    console.log(movie)
 
-  if (request.status >= 200 && request.status < 400) {
-    data.forEach(movie => {
-      console.log(movie.title);
-    });
-  } else {
-    console.log('error');
-  }
-}
+    const card = document.createElement('article')
 
-request.send();
+    const title = document.createElement('h1')
+          title.textContent = movie.title
 
-// 'use strict'
-//
-// var request = new XMLHttpRequest()
+    const text = document.createElement('p')
+          text.textContent = movie.description
+
+    app.appendChild(card)
+
+    card.appendChild(title)
+    card.appendChild(text)
+
+  })
+})
+
+.catch(function(error) {
+    console.log(error)
+})
+
+// var request = new XMLHttpRequest();
 // var url = 'https://ghibliapi.herokuapp.com/films'
+// var app = document.getElementById('main')
 //
 // request.open('GET', url, true)
 //
-// request.addEventListener("load", data())
+// request.onload = function () {
 //
-// function data () {
-//
-//   // Begin accessing JSON data here
 //   var data = JSON.parse(this.response)
 //
 //   if (request.status >= 200 && request.status < 400) {
 //     data.forEach(movie => {
-//       console.log(movie.title)
-//     });
+//       const card = document.createElement('article')
+//
+//       const title = document.createElement('h1')
+//       title.textContent = movie.title
+//
+//       const detail = document.createElement('a')
+//       detail.textContent = "detail pagina"
+//
+//       const text = document.createElement('p')
+//       text.textContent = movie.description
+//
+//       app.appendChild(card)
+//
+//       card.appendChild(title)
+//       card.appendChild(text)
+//
+//     })
 //   } else {
 //     console.log('error')
 //   }
